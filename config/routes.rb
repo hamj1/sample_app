79cc7dd6fafa1957601a3650f848c3df298c0a74
John Hammond <hamj1@umbc.edu>
2013-02-26 15:53:51 -0500

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 SampleApp::Application.routes.draw do
-  match '/help',    to: 'static_pages#help'
-  match '/about',   to: 'static_pages#about'
-  match '/contact', to: 'static_pages#contact' 
+  match '/static_pages/help'  =>  'static_pages#help'
+  match '/static_pages/about' =>  'static_pages#about'
+  match '/static_pages/contact'=> 'static_pages#contact' 
   match '/static_pages/home' => 'static_pages#home'
-
+  
+   
   root to: 'static_pages#home'
+  
 end
+
