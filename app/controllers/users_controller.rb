7dca14174e76906d5019d1ccd58e029c7afc4b4c
John Hammond <hamj1@umbc.edu>
2013-03-30 21:43:00 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+
+  def show
+   @user = User.find(params[:id])
+  end
+
   def new
+   @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        flash[:success] = "Welcome to the Sample App!"
+        # redirect_to @user
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        # render 'new'
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
