2828771fcf190b74d691c809a18f24543771550b
John Hammond <hamj1@umbc.edu>
2013-04-12 18:22:23 -0400

    Finish sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,19 @@
 class SessionsController < ApplicationController
 
- def new
+def new
  end
 
- def create
-   user = User.find_by_email(params[:email].downcase)
-   if user && user.authenticate(params[:password])
-     session[:user] = user.id
-     sign_in user
-     redirect_to user
-   else
-     flash.now[:error] = 'Invalid email/password combination'
-     render 'new'
-   end
- end
-
-
- def destroy
+ def sign_in(user)
+    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    @current_user = user
+  end
+ 
+def sign_out
+    cookies.delete(:remember_token)
+    @current_user = nil
+  end
+   
+def destroy
    sign_out
    redirect_to root_url
  end
